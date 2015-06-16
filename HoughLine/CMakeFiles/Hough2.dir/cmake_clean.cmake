f81453a51ecf475b92a233843dad6dc6820fa37c
Adrian <franco.adrin@gmail.com>
2015-06-16 12:32:34 -0500

    Added HougLine project

---
+++ HoughLine/CMakeFiles/Hough2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hough2.dir/Hough2.cpp.o"
+  "Hough2.pdb"
+  "Hough2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Hough2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ HoughLine/CMakeFiles/HoughLine.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HoughLine.dir/HoughLine.cpp.o"
+  "HoughLine.pdb"
+  "HoughLine"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HoughLine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
